834cdf493e62c91d9d7b60af9a710f6e5f0a6ac0
Oleg <stroleg912@gmail.com>
2022-09-09 13:11:37 +0300

    add change

--- src/js/main.js
+++ src/js/main.js
@@ -2,21 +2,3 @@ import {
   asd
 } from './func.js';
 asd();
-// import Swiper JS
-import Swiper, {
-  Navigation,
-  Pagination
-} from 'swiper';
-// import Swiper styles
-// import 'swiper/css';
-
-const swiper = new Swiper('.swiper', {
-  modules: [Navigation, Pagination],
-  // Optional parameters
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-});
